17ecb6b6e29dd89ca6ebe76b24878314b6ab6334
kennethfan <kennethfan137@gmail.com>
2024-02-19 10:17:28 +0800

    rocksdb test

---
+++ file/src/main/java/io/github/kennethfan/file/rocksdb/RocksdbMain.java
@@ -0,0 +1,52 @@
+package io.github.kennethfan.file.rocksdb;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+public class RocksdbMain {
+
+    private static final String path = System.getProperty("user.home") + "/data/crawldb";
+
+    public static void main(String[] args) throws Exception {
+        RocksdbClient rocksdbClient = RocksdbClient.newInstance(path);
+
+        rocksdbClient.put("hello", "world");
+        log.info("key=hello, value={}", rocksdbClient.get("hello", String.class));
+
+        rocksdbClient.put("hello", null);
+        log.info("key=hello, value={}", rocksdbClient.get("hello", String.class));
+
+        List<String> keys = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String key = UUID.randomUUID().toString().replace("-", "");
+            String value = UUID.randomUUID().toString().replace("-", "");
+            rocksdbClient.put(key, value);
+            if (i % 2 == 0) {
+                keys.add(key);
+            }
+        }
+
+        Iterator<RocksdbClient.RocksdbRow<String, String>> iterator = rocksdbClient.iterator(String.class, String.class);
+        int i = 0;
+        while (iterator.hasNext()) {
+            RocksdbClient.RocksdbRow<String, String> row = iterator.next();
+            log.info("i={}, key={}, value={}", i++, row.getKey(), row.getValue());
+        }
+
+        for (String key : keys) {
+            rocksdbClient.delete(key);
+        }
+
+        iterator = rocksdbClient.iterator(String.class, String.class);
+        i = 0;
+        while (iterator.hasNext()) {
+            RocksdbClient.RocksdbRow<String, String> row = iterator.next();
+            log.info("i={}, key={}, value={}", i++, row.getKey(), row.getValue());
+        }
+    }
+}
