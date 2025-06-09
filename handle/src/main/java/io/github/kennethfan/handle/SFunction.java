c1e5416912310fce4c14dad2ac5462f82a580a84
kennethfan <kennethfan137@gmail.com>
2025-06-09 17:37:50 +0800

    SerializedLambda

---
+++ handle/src/main/java/io/github/kennethfan/handle/SFunction.java
@@ -0,0 +1,8 @@
+package io.github.kennethfan.handle;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface SFunction<T, R> extends Function<T, R>, Serializable {
+}
